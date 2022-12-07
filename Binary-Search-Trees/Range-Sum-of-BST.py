d7bdf5a6a3f15a7dd837bc8bbeec4e70ba8df8c8
Jack LaVelle <jacklavelle17@gmail.com>
2022-12-07 18:17:00 -0500

    Simple reorganizing

---
+++ Binary-Search-Trees/Range-Sum-of-BST.py
@@ -0,0 +1,30 @@
+# Leetcode Daily Challenge 12/7/2022
+# 938. Range Sum of BST - EASY
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        return self.recursiveHelper(root, low, high)
+    
+    def recursiveHelper(self, node, low, high):
+        leftsum = 0
+        rightsum = 0
+        if node.val > low:
+            if node.left:
+                leftsum += self.recursiveHelper(node.left, low, high)
+
+        if node.val < high:
+            if node.right:
+                rightsum += self.recursiveHelper(node.right, low, high)
+            
+        totalsum = leftsum + rightsum
+        
+        if low <= node.val <= high:
+            totalsum += node.val
+
+        return totalsum
