fe532c0c2f3924596d368963b1b49fe72c9c5781
Jack LaVelle <108889300+jack-lavelle@users.noreply.github.com>
2022-12-09 16:12:45 -0600

    Create Maximum-Difference-Between-Node-and-Ancestor.py

---
+++ Binary-Search-Trees/Maximum-Difference-Between-Node-and-Ancestor.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        return self.recursiveHelper(root, float("-inf"), float("inf"))
+    def recursiveHelper(self, node, maxi, mini):
+        left = float("-inf")
+        right = float("-inf")
+        
+        maxi = max(node.val, maxi)
+        mini = min(node.val, mini)
+
+        if node.left:
+            left = self.recursiveHelper(node.left, maxi, mini)
+
+        if node.right:
+            right = self.recursiveHelper(node.right, maxi, mini)
+
+        return max(left, right, abs(maxi - node.val), abs(mini - node.val))
